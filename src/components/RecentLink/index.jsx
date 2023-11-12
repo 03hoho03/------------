d26f132e76f299af33d1b249a33400813d9f4a0b
03hoho03 <ghwjd321@naver.com>
2023-11-12 16:15:28 +0900

    Feat 유튜브요약페이지링크

---
+++ src/components/RecentLink/index.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const linkType = [
+  {
+    type: "youtube",
+    message: "최근 유튜브 요약본 보러가기",
+    path: "/recent/youtube",
+  },
+];
+const StyledLink = styled(Link)`
+  color: red;
+  font-size: 14px;
+  cursor: pointer;
+  &:hover {
+    text-decoration-line: underline;
+  }
+`;
+const RecentLink = ({ filter }) => {
+  const [linkInfo, setLinkInfo] = useState({});
+  useEffect(() => {
+    const filtered = linkType.filter((item) => item.type === filter);
+    setLinkInfo(filtered[0]);
+  }, []);
+  return <StyledLink to={linkInfo.path}>{linkInfo.message}</StyledLink>;
+};
+
+export default RecentLink;
