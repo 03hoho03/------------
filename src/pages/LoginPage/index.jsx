c2f31be747022dddd71ab8c62281ed6ecf2bb172
03hoho03 <ghwjd321@naver.com>
2023-11-12 14:42:17 +0900

    Feat 로그인페이지

---
+++ src/pages/LoginPage/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "../../components/Login/LoginForm";
+import PageContainer from "../../common/page";
+
+const MainPage = () => {
+  return (
+    <PageContainer>
+      <LoginForm />
+    </PageContainer>
+  );
+};
+
+export default MainPage;
