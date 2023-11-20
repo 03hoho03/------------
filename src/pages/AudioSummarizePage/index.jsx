711a533d64903bc2d3dc82a87fb4d34cff3c828b
03hoho03 <ghwjd321@naver.com>
2023-11-20 22:52:30 +0900

    feat 오디오요약 결과추가

--- src/pages/AudioSummarizePage/index.jsx
+++ src/pages/AudioSummarizePage/index.jsx
@@ -6,31 +6,36 @@ import useSummaryService from "../../api/useSummaryService";
 import { useRecoilValue } from "recoil";
 import { useNavigate } from "react-router-dom";
 import { userAtom } from "../../_state/user";
+import AudioSummarizeResult from "../../components/AudioSummarizeResult";
 
 const MainWrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 120px;
   width: 100%;
+  max-width: 800px;
   padding: 0 24px;
+  margin: 120px auto;
 `;
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+  width: 100%;
+  max-width: 320px;
   margin-bottom: 24px;
 `;
 const InputLabel = styled.label`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 320px;
+  width: 100%;
   height: 280px;
   border: 1px solid #ccc;
   border-radius: 8px;
   margin-bottom: 12px;
 `;
-const LabelBtn = styled.button`
+const LabelBtn = styled.label`
+  font-size: 14px;
   padding: 12px 24px;
   color: white;
   background-color: #ffa600d3;
@@ -38,11 +43,18 @@ const LabelBtn = styled.button`
   border-radius: 8px;
   &:hover {
     background-color: orange;
+    cursor: pointer;
   }
 `;
 const FileInput = styled.input`
   width: 0;
   height: 0;
+`;
+const FileName = styled.p`
+  min-height: 24px;
+  text-align: center;
+  margin-bottom: 8px;
+  overflow: hidden;
 `;
 const SubmitBtn = styled.button`
   color: white;
@@ -66,15 +78,26 @@ const AudioSummarizePage = () => {
   const navigate = useNavigate();
   const { isAuth } = useRecoilValue(userAtom);
   const [file, setFile] = useState(null);
+  const whisper = useQuery({
+    queryKey: ["whisper"],
+    queryFn: () => summaryService.getTextFromWhisper(file),
+    enabled: false,
+  });
+  const audioSummarize = useQuery({
+    queryKey: ["audioSummarize"],
+    queryFn: () => summaryService.audioSummarize(whisper.data),
+    enabled: false,
+  });
+  useEffect(() => {
+    if (whisper.isSuccess) {
+      audioSummarize.refetch();
+    }
+  }, [whisper.isSuccess]);
 
   const HandleSubmit = async (e) => {
     e.preventDefault();
     if (!file) return;
-    const WhisperResult = await summaryService.getTextFromWhisper(file);
-    const body = {
-      source_text: WhisperResult,
-    };
-    const summaryResult = await summaryService.audioSummarize(body);
+    whisper.refetch();
   };
   const HandleFileChange = (e) => {
     if (!isAuth) {
@@ -90,16 +113,20 @@ const AudioSummarizePage = () => {
       <FormHeader title={title} description={description} />
       <Form onSubmit={HandleSubmit}>
         <InputLabel htmlFor="fileInput">
-          <LabelBtn>오디오 파일 업로드</LabelBtn>
+          <LabelBtn htmlFor="fileInput">오디오 파일 업로드</LabelBtn>
+          <FileInput
+            id="fileInput"
+            type="file"
+            accept="audio/*"
+            onChange={HandleFileChange}
+          />
         </InputLabel>
-        <FileInput
-          id="fileInput"
-          type="file"
-          accept="audio/*"
-          onChange={HandleFileChange}
-        />
+        <FileName>{file?.name && `현재 파일: ${file.name}`}</FileName>
         <SubmitBtn type="submit">Summarize</SubmitBtn>
       </Form>
+      {audioSummarize.isSuccess && audioSummarize.data && (
+        <AudioSummarizeResult summaryData={audioSummarize.data} />
+      )}
     </MainWrapper>
   );
 };
